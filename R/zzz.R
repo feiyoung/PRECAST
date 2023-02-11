2ecd95ddbe96ce53dd08e1b4b631943df01f7a24
feiyoung <1029725413@qq.com>
2023-02-11 15:52:36 +0800

    'update'

--- R/zzz.R
+++ R/zzz.R
@@ -2,5 +2,6 @@
   # output the specific message when package is loaded!
   RFver <- read.dcf(file=system.file("DESCRIPTION", package=pkgname),
                     fields=c("Description"))
-  packageStartupMessage(paste(pkgname,': ', RFver))
+  website <-  "Check out our Package website (https://feiyoung.github.io/PRECAST/index.html) for a more complete description of the methods and analyses"
+  packageStartupMessage(paste(pkgname,': ', RFver, " ", website))
 }
